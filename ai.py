dea0a2f7d4609c03b7ab327cc4ea50b5b1b7760c
Jakob Unnebäck <jakob@folkdatorn.se>
2023-03-24 13:07:41 +0100

    Initial commit

---
+++ ai.py
@@ -0,0 +1,141 @@
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import time
+from data import getBatch
+
+
+n_epochs = 1
+batch_size_train = 1024
+mini_batch_size_train = 128
+batch_size_test = 1024
+learning_rate = 0.000005
+momentum = 0.5
+log_interval = 30
+
+# random_seed = 1
+random_seed = torch.randint(0, 100000, (1,)).item()
+torch.backends.cudnn.enabled = False
+torch.manual_seed(random_seed)
+
+
+# examples = enumerate(test_loader)
+# batch_idx, (example_data, example_targets) = next(examples)
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.linearSize = self.getLinearSize()
+        print('Linear size: ' + str(self.linearSize))
+        self.fc1 = nn.Linear(self.linearSize, 50)
+        self.fc2 = nn.Linear(50, 2)
+        
+    def getLinearSize (self):
+      testMat = torch.zeros(1, 2)
+      testMat = self.convForward(testMat)
+      testMat = testMat.flatten()
+      size = testMat.size().numel()
+      return size
+
+    def convForward(self, x) -> torch.Tensor:
+      return x
+
+    def forward(self, x):
+        x = self.convForward(x)
+        x = x.view(-1, self.linearSize)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+    def trainEpoch(self, epoch):
+      network.train()
+
+      # global train_loader
+      global train_loader
+
+      # get first data sample in enumarate order from train loader
+      batch_idx = 0
+      NUMBER_OF_BATCHES = 100
+
+      index = 0
+
+      while index < NUMBER_OF_BATCHES:
+        [data, target] = getBatch(batch_size_train)
+
+        # [data, target] = uniform(data, target, mini_batch_size_train)
+
+        optimizer.zero_grad()
+        output = network(data)
+        loss = F.cross_entropy(output, target)
+        loss.backward()
+        optimizer.step()
+        
+        if batch_idx % log_interval == 0:
+          acc = self.test()
+          accPlot.append(acc)
+          lossPlot.append(loss.item())
+          # plot(accPlot, None)
+
+          # end time
+          end = time.time()
+
+          train_losses.append(loss.item())
+      
+        batch_idx += 1
+
+      
+
+    def test(self):
+      network.eval()
+      test_loss = 0
+      correct = 0
+      total = 0
+      # NUMBER_OF_BATCHES = int(test_loader.dataset['x'].shape[0] / mini_batch_size_train)
+      NUMBER_OF_BATCHES = 20
+      with torch.no_grad():
+        batchIndex = 0
+
+
+        while batchIndex < NUMBER_OF_BATCHES:
+          [data, target] = getBatch(batch_size_test)
+
+
+        # for data, target in test_loader:
+          output = network(data)
+          test_loss += F.nll_loss(output, target, reduction='sum').item()
+          pred = output.data.max(1, keepdim=True)[1]
+          correct += pred.eq(target.data.view_as(pred)).sum()
+          total += target.size(0)
+
+          batchIndex = batchIndex + 1
+
+
+      test_loss /= total
+      test_losses.append(test_loss)
+      print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, total,
+        100. * correct / total))
+      accTensor = correct / total
+      return accTensor.item()
+
+network = Net()
+# optimizer = optim.SGD(network.parameters(), lr=learning_rate, momentum=momentum)
+optimizer = optim.Adam(network.parameters(), lr=learning_rate)
+
+train_losses = []
+train_counter = []
+test_losses = []
+
+accPlot = []
+lossPlot = []
+print('Starting training')
+
+
+for epoch in range(1, n_epochs + 1):
+  network.trainEpoch(epoch)
+
+# nodes = networkTo3dNodes(network, HEIGHT, WIDTH, CHANNELS)
+# log3DNodes(nodes, 'camyleon - mini')
---
+++ data.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+def getBatch(size: int) -> list[torch.Tensor, torch.Tensor]:
+    # create torch tensor of size (size, 2)
+    data = torch.zeros(size, 2, dtype=torch.float)
+    # fill tensor with random values between -1 and 1
+    data.uniform_(-1, 1)
+    # create target tensor
+    target = torch.zeros(size, dtype=torch.float)
+    # fill target tensor with 1 if x^2 + y^2 < 1 else 0
+    target = torch.where(data.pow(2).sum(1) < 1, torch.ones(size), torch.zeros(size))
+
+    # convert to long
+    target = target.long()
+    data = data.float()
+    return [data, target]
+
